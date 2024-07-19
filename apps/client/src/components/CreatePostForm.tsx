d6a875ce03877e79d3685b403c3e8ca66a8c96fc
Carson Weeks <mail@carsonweeks.com>
2024-07-19 15:56:15 -0400

    Added tripcode to form

--- apps/client/src/components/CreatePostForm.tsx
+++ apps/client/src/components/CreatePostForm.tsx
@@ -9,6 +9,8 @@ const CreatePostForm = () => {
     defaultValues: {
       content: "",
       media: undefined as undefined | File,
+      username: "",
+      password: "",
     },
     onSubmit: async ({ value, formApi }) => {
       formApi.reset()
@@ -17,7 +19,6 @@ const CreatePostForm = () => {
       window.location.href = res.redirect
       window.location.reload()
     },
-    onSubmitInvalid: async ({ value, formApi }) => {},
   }))
 
   return (
@@ -34,16 +35,12 @@ const CreatePostForm = () => {
         <div>
           <form.Field
             name="content"
-            validators={{
-              onSubmitAsync: async ({ value }) => {
-                return value.length < 6 ? "error" : undefined
-              },
-            }}
             children={(field) => (
               <>
                 <label for={field().name}>Content</label>
                 <input
                   name={field().name}
+                  id={field().name}
                   value={field().state.value}
                   onInput={(e) => field().handleChange(e.target.value)}
                 />
@@ -57,6 +54,15 @@ const CreatePostForm = () => {
         <div>
           <form.Field
             name="media"
+            validators={{
+              onChangeListenTo: ["content"],
+              onSubmit: ({ value, fieldApi }) => {
+                const content = fieldApi.form.getFieldValue("content")
+                if (!content && !value)
+                  return "Either set content or add media."
+                return undefined
+              },
+            }}
             children={(field) => (
               <>
                 <label for={field().name}>Media</label>
@@ -64,8 +70,9 @@ const CreatePostForm = () => {
                   name={field().name}
                   type="file"
                   ref={fileInput}
+                  id={field().name}
                   accept="image/png, image/jpeg"
-                  onInput={(e) =>
+                  onChange={(e) =>
                     field().handleChange(e.target.files?.item(0) ?? undefined)
                   }
                 />
@@ -83,6 +90,63 @@ const CreatePostForm = () => {
                     Clear
                   </button>
                 </div>
+              </>
+            )}
+          />
+        </div>
+        <div>
+          <form.Field
+            name="username"
+            validators={{
+              onChangeListenTo: ["password"],
+              onSubmit: ({ value, fieldApi }) => {
+                const password = fieldApi.form.getFieldValue("password")
+                if (password && !value)
+                  return "If setting a password, set a username."
+                return undefined
+              },
+            }}
+            children={(field) => (
+              <>
+                <label for={field().name}>Username</label>
+                <input
+                  name={field().name}
+                  id={field().name}
+                  value={field().state.value}
+                  onInput={(e) => field().handleChange(e.target.value)}
+                />
+                {field().state.meta.errors ? (
+                  <em role="alert">{field().state.meta.errors.join(", ")}</em>
+                ) : null}
+              </>
+            )}
+          />
+        </div>
+        <div>
+          <form.Field
+            name="password"
+            validators={{
+              onChangeListenTo: ["username"],
+              onSubmit: ({ value, fieldApi }) => {
+                const username = fieldApi.form.getFieldValue("username")
+                if (username && !value)
+                  return "If setting a username, set a password."
+                return undefined
+              },
+            }}
+            children={(field) => (
+              <>
+                <label for={field().name}>Password</label>
+                <input
+                  name={field().name}
+                  id={field().name}
+                  type="password"
+                  value={field().state.value}
+                  onInput={(e) => field().handleChange(e.target.value)}
+                />
+                {field().state.meta.errors ? (
+                  <em role="alert">{field().state.meta.errors.join(", ")}</em>
+                ) : null}
               </>
             )}
           />
